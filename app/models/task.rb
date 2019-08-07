eef2d7fe3a73a8e6543079d739d8e8a8a3ddfec1
Platform45-Nic <nicolo@platform45.com>
2019-08-07 18:27:26 +0200

    Added scope methods to tasks model. Changed the index view to use scope methods instead of Task.all. Moved lists resource in routes to be nested inside tasks.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
-  scope :done, -> { where(done: true)}
+
+  def self.done
+    where(done: true).all
+  end
+
+  def self.not_done
+    where(done: false).all
+  end
+
 end
